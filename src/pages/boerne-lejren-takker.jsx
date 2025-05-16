0df3cb8b8ac8e4f7493cb5bacf65afd006c87017
BingbingDawg <U87960@rts-365.dk>
2025-05-16 10:13:24 +0200

    added styling to sponsor and thank you page

--- src/pages/boerne-lejren-takker.jsx
+++ src/pages/boerne-lejren-takker.jsx
@@ -1,21 +1,24 @@
 import HeaderComponent from "../components/headercomponent";
 import Thankyou from "../assets/images/fotos/boernelejren-takker.jpg";
+import "../sass/boerntakker.scss";
 
 export default function LejreTakker() {
 
     return (
         <>
-        <HeaderComponent />
-        <h2 className="siteheading">Børnelejren takker</h2>
-        <img src={Thankyou} alt="" />
-        <p>
-            Børnelejren på Langeland takker alle, der på den ene eller anden måde, har støttet
-            foreningens arbejde med at sende dårligt stillede børn på et ophold på Søgård Hovedgård
-            - det være sig ved naturaliesponsorater eller økonomisk støtte fra støttemedlemmer,
-            virksomhedssponsorer og donationer fra fonde.
-            En særlig tak til:
-        </p>
-         
+            <HeaderComponent />
+            <h2 className="siteheading">Børnelejren takker</h2>
+            <div className="sponsorlist">
+                <img className="sponsorlist__image" src={Thankyou} alt="" />
+                <p className="sponsorlist__text">
+                    Børnelejren på Langeland takker alle, der på den ene eller anden måde, har støttet
+                    foreningens arbejde med at sende dårligt stillede børn på et ophold på Søgård Hovedgård
+                    - det være sig ved naturaliesponsorater eller økonomisk støtte fra støttemedlemmer,
+                    virksomhedssponsorer og donationer fra fonde.
+                    En særlig tak til:
+                </p>
+            </div>
+                <div className="sponsorbarrier"></div>
         </>
     )
 }
